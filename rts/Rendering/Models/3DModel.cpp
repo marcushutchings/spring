51f6b12b40d7089a8a04a39ee8cab62fc5603925
LHoG <1476261+lhog@users.noreply.github.com>
2021-01-06 13:57:44 +0300

    WIP4

--- rts/Rendering/Models/3DModel.cpp
+++ rts/Rendering/Models/3DModel.cpp
@@ -1,7 +1,7 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "3DModel.h"
-#include "ModelVBO.h"
+#include "ModelVBO.hpp"
 
 #include "Game/GlobalUnsynced.h"
 #include "Rendering/GL/myGL.h"
